4bb559eea42e7155faf7e19c08aeecbc1eb1b869
José Antonio Rodrigues Ribeiro <up202108868@fe.up.pt>
2023-11-19 10:59:22 +0000

    Add full-text search caabilities to the News_Item class

--- app/Models/News_Item.php
+++ app/Models/News_Item.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class News_Item extends Model
 {
@@ -37,5 +38,19 @@ class News_Item extends Model
         return $this
           ->belongsToMany(Project::class, 'vote', 'id_content', 'id_user')
           ->withPivot('vote');
+    }
+
+    public static function full_text_search($query){
+        $sub = (DB::table('news_item')
+                ->select('id')
+                ->from(DB::raw('news_item, plainto_tsquery(\'english\',?) query'))
+                ->whereRaw('tsvectors @@ query')
+                ->orderByRaw('ts_rank(tsvectors, query) desc')
+                ->setBindings([$query]));
+        return Content::joinSub($sub, 'news', function($join)
+            {
+                $join->on(DB::Raw('content.id'), '=', DB::Raw('news.id'));
+            }
+        );
     }
 }
