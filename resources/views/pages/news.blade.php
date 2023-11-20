c85e45d7d2e2623d38140aa9e8141b9970d5689f
José Antonio Rodrigues Ribeiro <up202108868@fe.up.pt>
2023-11-20 15:00:21 +0000

    Implement extendable feed api/js

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 
 <div class="feed_buttons">
-    <button class="feed_button top_feed">Top Feed</button>
+    <a class="button feed_button" href="{{ route('top_feed') }}">Top Feed</a>
     @if (Auth::check())
-    <button class="feed_button follow_feed">Following Feed</button>
+    <a class="button feed_button" href="{{ route('follow_feed') }}">Following Feed</a>
     @endif
-    <button class="feed_button recent_feed">Recent Feed</button>
+    <a class="button feed_button" href="{{ route('recent_feed') }}">Recent Feed</a>
 </div>
 <div class="all_news">
-    @include('partials.list_feed', ['news_list' => DB::table('news_item')->select(DB::raw('content.*, news_item.title, sum(vote.vote)'))->leftJoin('vote','vote.id_content','=','news_item.id')->join('content','content.id','=','news_item.id')->groupBy('news_item.id')->groupBy('content.id')->orderByRaw('sum(vote.vote) DESC'), 'perPage' => 10])
+    @include('partials.list_feed', ['news_list' => $news_list, 'perPage' => 10])
 </div>
 
 @endsection
