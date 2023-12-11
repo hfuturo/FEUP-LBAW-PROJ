0d686b41678280e86b4dcf011464e52420256725
Rita Leite <up202105309@fe.up.pt>
2023-12-11 18:21:52 +0000

    Implement notifications deletetion

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -16,4 +16,11 @@ class NotificationController extends Controller
         $notifications = Notified::where('id_notified','=',Auth::user()->id)->orderBy('date', 'desc');
         return view('pages.notification',['notifications' => $notifications]);
     }
+
+    public function destroy(Request $request) 
+    {
+        $delete = Notification::where('id', $request->input("notification"))->delete();
+        $response = ['id' => $request->input("notification")];
+        return response()->json($response);
+    }
 }
--- resources/views/pages/notification.blade.php
+++ resources/views/pages/notification.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.app')
 
 @section('head')
-    <link href="{{ url('css/info.css') }}" rel="stylesheet">
+    <script type="text/javascript" src={{ url('js/notification.js') }} defer></script>
 @endsection
 
 @section('content')
     <section class="info_section">
         @foreach ($notifications->paginate(10) as $notif)
-            <article class="user_news">
+            <article class="user_news" id="{{ $notif->notification->id }}">
+                <h4>
+                <span class="material-symbols-outlined icon_red notification_button">delete</span>
                 @if ($notif->notification->type === "follow")
-                    <p><a href="{{ route('profile', ['user' => $notif->notification->user]) }}">{{ $notif->notification->user->name }}</a> is following you ! {{ $notif->date}}</p>
-                @else
+                        <a href="{{ route('profile', ['user' => $notif->notification->user]) }}">{{ $notif->notification->user->name }}</a> is following you !
                 @endif
+                @if ($notif->notification->type === "content")
+                        <a href="{{ route('news_page', ['id' => $notif->notification->content->comments->news_item->id]) }}">{{ $notif->notification->content->comments->news_item->title }}</a> has a new comment, go check !
+                @endif
+                </h4>
+                <p>{{ $notif->date}}</p>
             </article>
         @endforeach
         <span>{{ $notifications->paginate(10)->links() }}</span>
