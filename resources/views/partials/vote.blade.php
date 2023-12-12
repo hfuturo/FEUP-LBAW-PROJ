ab4dddcae8d6b7760a986367890b83bfc20e5f4a
Rita Leite <up202105309@fe.up.pt>
2023-12-12 15:41:15 +0000

    Improve css of votes

--- resources/views/partials/vote.blade.php
+++ resources/views/partials/vote.blade.php
@@ -5,8 +5,13 @@
     @if ($vote)
         <input type="hidden" class="vote_value" name="vote_value" value="{{ $vote->vote }}">
     @endif
-    <button class='vote accept'><span class="material-symbols-outlined">thumb_up</span></button>
-    <p class="up_count">{{ $item->likes() }}</p>
-    <button class='vote remove'><span class="material-symbols-outlined">thumb_down</span></button>
-    <p class="down_count">{{ $item->dislikes() }}<p>
+    <button class='vote accept'>
+        <span class="material-symbols-outlined">thumb_up</span>
+        <span class="up_count">{{ $item->likes() }}</span>
+    </button>
+   
+    <button class='vote remove'>
+        <span class="material-symbols-outlined">thumb_down</span>
+        <span class="down_count">{{ $item->dislikes() }}</span>
+    </button>
 </div>
