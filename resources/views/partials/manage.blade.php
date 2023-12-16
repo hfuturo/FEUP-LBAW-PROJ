d629e7fb368f520e6c67712cbcb3dc63ca0976a8
Henrique Silva <hggfsilva@gmail.com>
2023-12-16 02:07:53 +0000

    fixed css

--- resources/views/partials/manage.blade.php
+++ resources/views/partials/manage.blade.php
@@ -1,10 +1,12 @@
 <li class="user" id="{{ $user->id }}">
     <a href="{{ route('profile', ['user' => $user->id]) }}">{{ $user->name }}</a>
-    @if ($user->blocked)
-        <button class="block" data-operation="unblock_user"><span class="material-symbols-outlined">done_outline
-            </span></button>
-    @else
-        <button class="block" data-operation="block_user"><span class="material-symbols-outlined">block
-            </span></button>
+    @if (Auth::user()->id !== $user->id)
+        @if ($user->blocked)
+            <button class="block" data-operation="unblock_user"><span class="material-symbols-outlined">done_outline
+                </span></button>
+        @else
+            <button class="block" data-operation="block_user"><span class="material-symbols-outlined">block
+                </span></button>
+        @endif
     @endif
 </li>
