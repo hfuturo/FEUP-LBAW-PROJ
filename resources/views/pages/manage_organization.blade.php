d222c4f1867be745b36b5f0fe6ecbc0889de4153
Henrique Silva <hggfsilva@gmail.com>
2023-12-21 23:30:16 +0000

    form to delete org

--- resources/views/pages/manage_organization.blade.php
+++ resources/views/pages/manage_organization.blade.php
@@ -7,25 +7,46 @@
 
 @section('content')
     <section id="manage_section">
-        <h2>Members of <a href="{{route('show_org', ['organization' => $organization->id])}}"> {{ $organization->name }} </a></h2>
+        <h2>Members of <a href="{{ route('show_org', ['organization' => $organization->id]) }}"> {{ $organization->name }}
+            </a></h2>
         <button onclick="openRequestOrg()">See new requests</button>
-        <form method="POST" action="{{ route('delete_organization',  ['organization' => $organization->id]) }}">
-            {{ csrf_field() }}
-            <button type="submit" class="remove">Delete Organization</button>
-        </form>
+        <button onclick="openDeleteOrg()" class="remove delete_organization">Delete Organization</button>
+
+        <script>
+            function openDeleteOrg() {
+                Swal.fire({
+                    title: "Are you sure you want to delete this organization?",
+                    html: `
+        <form method="POST" action="{{ route('delete_organization', ['organization' => $organization->id]) }}">
+        {{ csrf_field() }}
+        </form> 
+                `,
+                    text: "You won't be able to revert this action.",
+                    icon: "warning",
+                    showCancelButton: true,
+                    preConfirm: () => {
+                        const popup = Swal.getPopup()
+                        const form = popup.querySelector("form")
+                        form.submit()
+                    }
+                })
+            }
+        </script>
+
         @include('partials.list_requests_organization')
         <input type="hidden" id="org" name="org" value="{{ $organization->id }}">
         @foreach ($organization->members as $member)
-        <article class="info_article" id="{{$member->id_user}}">
-            <h4><a href="{{ route('profile', ['user' => $member->id]) }}">{{ $member->name }}</a><span class="role">({{ $member->member_type }})</span></h4>
-            <p>Since: {{ \Carbon\Carbon::parse($member->joined_date) }}</p>
-            @if($member->member_type !== "leader")
-            <div>
-                <button class="button manage expel" data-operation="expel">Expel</button>
-                <button class="button manage upgrade" data-operation="upgrade">Upgrade</button>
-            </div>
-            @endif
-        </article>
+            <article class="info_article" id="{{ $member->id_user }}">
+                <h4><a href="{{ route('profile', ['user' => $member->id]) }}">{{ $member->name }}</a><span
+                        class="role">({{ $member->member_type }})</span></h4>
+                <p>Since: {{ \Carbon\Carbon::parse($member->joined_date) }}</p>
+                @if ($member->member_type !== 'leader')
+                    <div>
+                        <button class="button manage expel" data-operation="expel">Expel</button>
+                        <button class="button manage upgrade" data-operation="upgrade">Upgrade</button>
+                    </div>
+                @endif
+            </article>
         @endforeach
     </section>
 @endsection
