8c7b3866d8a1faa202e45ea39b72505f963a7f8a
Rita Leite <up202105309@fe.up.pt>
2023-12-21 15:53:56 +0000

    Change popup in list members

--- public/js/organization.js
+++ public/js/organization.js
@@ -1,11 +1,3 @@
-function openMembersOrg() {
-    document.getElementById("members_org_popup").style.display = "block";
-}
-
-function closeMembersOrg() {
-    document.getElementById("members_org_popup").style.display = "none";
-}
-
 function openRequestOrg() {
     document.getElementById("request_org_popup").style.display = "block";
 }
